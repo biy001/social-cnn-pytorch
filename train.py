d9cbb07fcc0637f6cf07d256c3b3c0a3a83bf273
bilany <bilan.yang.llx@gmail.com>
2018-12-10 03:20:22 -0800

    Update train.py

--- train.py
+++ train.py
@@ -25,11 +25,11 @@ answer = None
 while answer not in ('y', 'n'):
     answer = input('Do you want to force preprocessing data? (y/n)')
     if answer == 'y':
-    	print('Confirmed forcePreProcess=True')
+        print('Confirmed forcePreProcess=True')
         if_force_preprocess = True
 
     elif answer == 'n':
-    	print('Confirmed forcePreProcess=False')
+        print('Confirmed forcePreProcess=False')
         if_force_preprocess = False
     else:
         print('Please enter y or n')
@@ -37,9 +37,9 @@ answer = None
 while answer not in ('y', 'n'):
     answer = input('Are you using the correct data pipeline to load train, dev, test sets? (y/n)')
     if answer == 'y':
-    	print('Great')
+        print('Great')
     elif answer == 'n':
-    	sys.exit('Execution stopped: please check')
+        sys.exit('Execution stopped: please check')
     else:
         print('Please enter y or n')
 
@@ -47,13 +47,16 @@ while answer not in ('y', 'n'):
 # from input_pipeline_fill_0 import CustomDataPreprocessorForCNN, CustomDatasetForCNN
 # from input_pipeline_mix_all_data import CustomDataPreprocessorForCNN, CustomDatasetForCNN
 # from input_pipeline_fill_0_mix_all_data import CustomDataPreprocessorForCNN, CustomDatasetForCNN
-from input_pipeline_individual_pedestrians_mix_all_data import CustomDataPreprocessorForCNN, CustomDatasetForCNN
+# from input_pipeline_individual_pedestrians_mix_all_data import CustomDataPreprocessorForCNN, CustomDatasetForCNN
+from input_pipeline_individual_pedestrians import CustomDataPreprocessorForCNN, CustomDatasetForCNN
+
 
 
 # Data preprocessor.
 # processor = CustomDataPreprocessorForCNN(test_data_sets = [35], dev_ratio_to_test_set = 0.5, forcePreProcess=if_force_preprocess)
-# processor = CustomDataPreprocessorForCNN(forcePreProcess=if_force_preprocess, test_data_sets=[30,35], dev_ratio_to_test_set = 0.8, augmentation=True)
-processor = CustomDataPreprocessorForCNN(dev_ratio=0.1, test_ratio=0.1, forcePreProcess=if_force_preprocess, augmentation=True)
+processor = CustomDataPreprocessorForCNN(forcePreProcess=if_force_preprocess, test_data_sets=[30,35], dev_ratio_to_test_set = 0.5, augmentation=True)
+# processor = CustomDataPreprocessorForCNN(dev_ratio=0.1, test_ratio=0.1, forcePreProcess=if_force_preprocess, augmentation=True)
+
 
 # Processed datasets. (training/dev/test)
 train_set = CustomDatasetForCNN(processor.processed_train_data_file)
@@ -62,40 +65,40 @@ test_set = CustomDatasetForCNN(processor.processed_test_data_file)
 
 
 def nonzero_row_index(inp): # inp is a 2m X T tensor
-	if inp.size()[0]%2 == 1:
-		sys.exit('Execution stopped at nonzero_row_index(): Error: 2m % 2 == 0 is not satisfied')
-	sr = torch.sum(inp, dim=1) # size = [nrow, 1] # nrow must be an even number
-	sr_ind_tensor = (sr != 0).nonzero() # nonzero index
-	ind_in_1Darray =  sr_ind_tensor.numpy().ravel() # index in 1Darray
-	insert_at = []
-	insert_value = []
-	append_value = False
-	for i in range(ind_in_1Darray.shape[0]):
-		if ind_in_1Darray[i]%2 == 0: # an even index means an x coordinate
-			if i == (ind_in_1Darray.shape[0] - 1):
-				append_value = True
-			elif ind_in_1Darray[i+1] != (ind_in_1Darray[i] + 1):
-				insert_at.append(i+1)
-				insert_value.append(ind_in_1Darray[i] + 1)
-		else: # an odd index means a y coordinate
-			if ind_in_1Darray[i-1] != (ind_in_1Darray[i] - 1):
-				insert_at.append(i)
-				insert_value.append(ind_in_1Darray[i] - 1)
-	new_ind = np.insert(ind_in_1Darray, insert_at, insert_value)
-	if append_value:
-		new_ind = np.append(new_ind, new_ind[-1]+1)
-	return new_ind
+    if inp.size()[0]%2 == 1:
+        sys.exit('Execution stopped at nonzero_row_index(): Error: 2m % 2 == 0 is not satisfied')
+    sr = torch.sum(inp, dim=1) # size = [nrow, 1] # nrow must be an even number
+    sr_ind_tensor = (sr != 0).nonzero() # nonzero index
+    ind_in_1Darray =  sr_ind_tensor.numpy().ravel() # index in 1Darray
+    insert_at = []
+    insert_value = []
+    append_value = False
+    for i in range(ind_in_1Darray.shape[0]):
+        if ind_in_1Darray[i]%2 == 0: # an even index means an x coordinate
+            if i == (ind_in_1Darray.shape[0] - 1):
+                append_value = True
+            elif ind_in_1Darray[i+1] != (ind_in_1Darray[i] + 1):
+                insert_at.append(i+1)
+                insert_value.append(ind_in_1Darray[i] + 1)
+        else: # an odd index means a y coordinate
+            if ind_in_1Darray[i-1] != (ind_in_1Darray[i] - 1):
+                insert_at.append(i)
+                insert_value.append(ind_in_1Darray[i] - 1)
+    ew_ind = np.insert(ind_in_1Darray, insert_at, insert_value)
+    if append_value:
+        new_ind = np.append(new_ind, new_ind[-1]+1)
+    return new_ind
 
 def delete_all_zero_rows(dataset_pair): # any data set from CustomDatasetForCNN 
-	new_data_pair = []
-	for input_output_pair in tqdm(dataset_pair):
-		data_input, data_output = input_output_pair[0], input_output_pair[1]
-		nonzero_ind = np.intersect1d(nonzero_row_index(data_input), nonzero_row_index(data_output))
-		if nonzero_ind.size == 0: # if there is no nonzero rows
-			continue
-		else:
-			new_data_pair.append((data_input[nonzero_ind,:], data_output[nonzero_ind,:]))
-	return new_data_pair
+    new_data_pair = []
+    for input_output_pair in tqdm(dataset_pair):
+        data_input, data_output = input_output_pair[0], input_output_pair[1]
+        nonzero_ind = np.intersect1d(nonzero_row_index(data_input), nonzero_row_index(data_output))
+        if nonzero_ind.size == 0: # if there is no nonzero rows
+            continue
+        else:
+            new_data_pair.append((data_input[nonzero_ind,:], data_output[nonzero_ind,:]))
+    return new_data_pair
 
 
 answer = None
@@ -103,23 +106,23 @@ while answer not in ('y', 'n'):
     answer = input('Do you want to delete all-0 rows and re-dump the trajectory pickle files? (y/n)')
     if answer == 'y':
         print('Deleting all-0 rows and re-dumping...')
-		new_dev_set = delete_all_zero_rows(dev_set)
-		print('--> Dumping dev data with size ' + str(len(new_dev_set)) + ' to pickle file')
-		f_dev = open(processor.processed_dev_data_file, 'wb')
-		pickle.dump(new_dev_set, f_dev, protocol=2)
-		f_dev.close()
+        new_dev_set = delete_all_zero_rows(dev_set)
+        print('--> Dumping dev data with size ' + str(len(new_dev_set)) + ' to pickle file')
+        f_dev = open(processor.processed_dev_data_file, 'wb')
+        pickle.dump(new_dev_set, f_dev, protocol=2)
+        f_dev.close()
 
-		new_test_set = delete_all_zero_rows(test_set)
-		print('--> Dumping test data with size ' + str(len(new_test_set)) + ' to pickle file')
-		f_test = open(processor.processed_test_data_file, 'wb')
-		pickle.dump(new_test_set, f_test, protocol=2)
-		f_test.close()
+        new_test_set = delete_all_zero_rows(test_set)
+        print('--> Dumping test data with size ' + str(len(new_test_set)) + ' to pickle file')
+        f_test = open(processor.processed_test_data_file, 'wb')
+        pickle.dump(new_test_set, f_test, protocol=2)
+        f_test.close()
 
-		new_train_set = delete_all_zero_rows(train_set)
-		print('--> Dumping train data with size ' + str(len(new_train_set)) + ' to pickle file')
-		f_train = open(processor.processed_train_data_file, 'wb')
-		pickle.dump(new_train_set, f_train, protocol=2)
-		f_train.close()
+        new_train_set = delete_all_zero_rows(train_set)
+        print('--> Dumping train data with size ' + str(len(new_train_set)) + ' to pickle file')
+        f_train = open(processor.processed_train_data_file, 'wb')
+        pickle.dump(new_train_set, f_train, protocol=2)
+        f_train.close()
 
     elif answer == 'n':
         print(' ')

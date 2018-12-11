7086a27b67c938f00b8a9484611b22a3e9493782
bilany <bilan.yang.llx@gmail.com>
2018-12-11 03:08:45 -0800

    Update plot_loss.py

--- plot_loss.py
+++ plot_loss.py
@@ -10,24 +10,31 @@ import matplotlib.pyplot as plt
 
 # loss_file_name = 'try_copy.txt'
 loss_folder = 'log/'
+# loss_file_name = loss_folder + 'before_dropout_0.01L2/train_errors_per_epoch_excluding_testset_[2].txt'
 loss_file_name = loss_folder + 'train_errors_per_epoch_excluding_testset_[2].txt'
+
 
 
 all_loss = np.loadtxt(loss_file_name, delimiter=',')
 # print(all_loss)
 
 # end_ep = 31
+start = 0
 end_ep = all_loss.shape[0] + 1
-ep = all_loss[:end_ep,0]
+
+# end_ep = 500
+
+ep = all_loss[start:end_ep,0]
 fig = plt.figure()
 
 ax1 = fig.add_subplot(1, 2, 1)
 ax2 = fig.add_subplot(1, 2, 2)
 
-ax1.plot(ep, all_loss[:end_ep,1], 'r--', label='average train error')
-ax1.plot(ep, all_loss[:end_ep,2], 'b--', label='dev error')
-ax2.plot(ep, all_loss[:end_ep,3], 'g--', label='displacement error')
-ax2.plot(ep, all_loss[:end_ep,4], 'y--', label='final dispplacement error')
+
+ax1.plot(ep, all_loss[start:end_ep,1], 'r--', label='average train error')
+ax1.plot(ep, all_loss[start:end_ep,2], 'b--', label='dev error')
+ax2.plot(ep, all_loss[start:end_ep,3], 'g--', label='displacement error')
+ax2.plot(ep, all_loss[start:end_ep,4], 'y--', label='final dispplacement error')
 
 fig.suptitle("Error history in training")
 ax1.grid(True)
@@ -37,12 +44,4 @@ ax2.legend(loc='upper center')
 ax1.set_xlabel('Epoch')
 ax2.set_xlabel('Epoch')
 plt.show()
-
-
-
-
-
-
-
-
 
